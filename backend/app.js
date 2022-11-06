5ccc68e22491b3d31cdb7751e0e97f16fd642fbd
dpdahal <dp48dahal@gmail.com>
2022-11-06 07:23:02 +0545

    project day1

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import connectDB from "./configuration/connection.js";
+
+
+import indexRouter from './routes/index.js';
+
+let app = express();
+
+connectDB().then((res) => {
+    console.log(res.message);
+}).catch((err) => {
+    console.log(err.message);
+});
+
+const __dirname = path.resolve();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+export default app;
